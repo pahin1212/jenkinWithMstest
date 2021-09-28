f4e0ccf3dfda24bffadbbcd79c19e38533dcbc77
Priyanka ahinave <priyanka.kakad@gmail.com>
2021-09-28 18:54:01 -0500

    second commit

---
+++ UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace testJenkinMsTestGit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            Assert.Fail("Test 1  failed");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            Assert.IsTrue(true, "test passed");
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+
+            Assert.Fail("Test 3  failed");
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+
+            Assert.IsTrue(true, "test passed");
+        }
+
+    }
+}
